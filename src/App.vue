2d61ec372e993aae361aec11b860a01ff3954662
消灭人类暴政\浪荡 <811654717@qq.com>
2023-04-12 18:03:40 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="video-content">
+      <h2>使用video.js 在网站中搭建视频</h2>
+      <h3>卡拉云——低代码开发工具，1 秒搭建上传后台</h3>
+      <player-video :volume="volume" ref="video" :src="src"></player-video>
+    </div>
+    <div class="button-group">
+      <el-button class="primary" @click="playVideo">开始视频</el-button>
+      <el-button class="primary" @click="stopVideo">暂停视频</el-button>
+      <el-button class="primary" @click="reloadVideo">重新加载</el-button>
+      <el-button class="primary" @click="forwardVideo">视频快进</el-button>
+      <el-button class="primary" @click="backVideo">视频后退</el-button>
+      <el-button class="primary" @click="volumeUpVideo">增大音量</el-button>
+      <el-button class="primary" @click="volumeDownVideo">降低音量</el-button>
+      <el-button class="primary" @click="toggleToFenghuangwang"
+        >凤凰卫视</el-button
+      >
+      <el-button class="primary" @click="toggleToDefault">默认频道</el-button>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import PlayerVideo from "./components/PlayerVideo.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    components: {
+      PlayerVideo,
+    },
+    data() {
+      return {
+        volume: 0.5,
+        src: "http://vjs.zencdn.net/v/oceans.mp4",
+      };
+    },
+    computed: {
+      video() {
+        return this.$refs.video;
+      },
+    },
+    methods: {
+      // 父类组件调用子组件方法，触发播放器功能
+      stopVideo() {
+        this.video.stop();
+      },
+      playVideo() {
+        this.video.play();
+      },
+      reloadVideo() {
+        this.video.reload();
+      },
+      forwardVideo() {
+        this.video.forward();
+      },
+      backVideo() {
+        this.video.back();
+      },
+      fullScreenVideo() {
+        this.video.fullScreen();
+      },
+      screenVideo() {
+        this.video.exitScreen();
+      },
+      volumeUpVideo() {
+        this.video.volumeUp();
+      },
+      volumeDownVideo() {
+        this.video.volumeDown();
+      },
+      toggleToFenghuangwang() {
+        this.video.toggleTv({
+          src: {
+            type: "application/x-mpegURL",
+            src: "https://playtv-live.ifeng.com/live/06OLEGEGM4G.m3u8",
+          },
+          load: "https://playtv-live.ifeng.com/live/06OLEGEGM4G.m3u8",
+        });
+      },
+      toggleToDefault() {
+        this.video.toggleTv({
+          src: {
+            type: "video/mp4",
+            src: "http://vjs.zencdn.net/v/oceans.mp4",
+          },
+          load: "http://vjs.zencdn.net/v/oceans.mp4",
+        });
+      },
+    },
+    beforeCreate() {
+      this.volume = window.localStorage.volume;
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    .video-content {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      margin-right: 20px;
+    }
+    .button-group {
+      margin-top: 20px;
+      display: flex;
+      flex: 0 0 100px;
+      flex-direction: column;
+      justify-content: space-between;
+      // align-items: center;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+Vue.prototype.$video = videojs;
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
